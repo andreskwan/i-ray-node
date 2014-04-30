bbe1633d44d89225c491c1ba58062c54dbb3d2b1
ANDRES KWAN <andreskwan@gmail.com>
2014-04-30 18:07:01 -0500

    ray - starting from zero

---
+++ TourMyTown/TourMyTown/Locations.m
@@ -0,0 +1,89 @@
+//
+//  Locations.m
+//  TourMyTown
+//
+//  Created by Michael Katz on 8/15/13.
+//  Copyright (c) 2013 mikekatz. All rights reserved.
+//
+
+#import "Locations.h"
+#import "Location.h"
+
+static NSString* const kBaseURL = @"http://localhost:3000/";
+static NSString* const kLocations = @"locations";
+static NSString* const kFiles = @"files";
+
+
+@interface Locations ()
+@property (nonatomic, strong) NSMutableArray* objects;
+@end
+
+@implementation Locations
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        _objects = [NSMutableArray array];
+    }
+    return self;
+}
+
+- (NSArray*) filteredLocations
+{
+    return [self objects];
+}
+
+- (void) addLocation:(Location*)location
+{
+    [self.objects addObject:location];
+}
+
+- (void)loadImage:(Location*)location
+{
+}
+
+- (void)parseAndAddLocations:(NSArray*)locations toArray:(NSMutableArray*)destinationArray
+{
+}
+
+- (void)import
+{
+    NSURL* url = [NSURL URLWithString:[kBaseURL stringByAppendingPathComponent:kLocations]]; //1
+    
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"GET"; //2
+    [request addValue:@"application/json" forHTTPHeaderField:@"Accept"]; //3
+    
+    NSURLSessionConfiguration* config = [NSURLSessionConfiguration defaultSessionConfiguration]; //4
+    NSURLSession* session = [NSURLSession sessionWithConfiguration:config];
+    
+    NSURLSessionDataTask* dataTask = [session dataTaskWithRequest:request completionHandler:
+            ^(NSData *data,
+              NSURLResponse *response,
+              NSError *error) { //5
+        if (error == nil) {
+            NSArray* responseArray = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL]; //6
+            [self parseAndAddLocations:responseArray
+                               toArray:self.objects]; //7
+        }
+    }];
+    
+    [dataTask resume]; //8
+}
+
+- (void) runQuery:(NSString *)queryString
+{
+ 
+}
+
+- (void) queryRegion:(MKCoordinateRegion)region
+{
+    
+}
+
+
+- (void) persist:(Location*)location
+{
+ 
+}@end
